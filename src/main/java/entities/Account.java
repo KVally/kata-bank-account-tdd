65bb474e91afcde7858174eff5a3d86185f59b1b
Vally KONATE <kvallyjunior@gmail.com>
2023-01-06 16:25:17 +0000

    implementation of withdrawal method

--- src/main/java/entities/Account.java
+++ src/main/java/entities/Account.java
@@ -1,6 +1,8 @@
 package entities;
 
 import exceptions.AmountFormatException;
+import exceptions.InsufficientFundsException;
+import exceptions.ValidationMessages;
 
 import java.math.BigDecimal;
 import java.util.LinkedHashSet;
@@ -39,6 +41,20 @@ public class Account {
     }
 
     public void withdrawal(BigDecimal amount) {
+        if (amount == null || amount.compareTo(BigDecimal.ZERO)<0) {
+            throw new AmountFormatException();
+        }
+        if (this.balance.compareTo(amount)<0) {
+            throw new InsufficientFundsException();
+        }
+        var transaction = new Transaction(
+                this.accountNumber,
+                this.balance,
+                amount,
+                Transaction.TransactionType.WITHDRAW
+        );
+        transactions.add(transaction);
+        balance = balance.subtract(amount);
     }
 
     public String printStatement() {
